df8350fb3b91b0579e7cf15ba2386d9ff0cbdfd2
mayankk-1804 <mayankmathur1804@gmail.com>
2026-02-27 21:32:11 +0530

    fixed linkedin link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration: 0.5}} >Mayank Mathur</motion.span>
             <div className="social">
                 <a href="https://github.com/mayankk-1803" target='_blank'><img src="/github.webp" alt="github" /></a>
-                <a href="www.linkedin.com/in/mayank-mathur-5095262aa" target='_blank'><img src="/linkedin.jpg" alt="linkedin" /></a>
+                <a href="https://www.linkedin.com/in/mayank-mathur-5095262aa/" target='_blank'><img src="/linkedin.jpg" alt="linkedin" /></a>
                 <a href="https://www.instagram.com/mayank15_k?igsh=MWRncm4wb3Jtc2F2MA%3D%3D&utm_source=qr" target='_blank'><img src="/instagram.png" alt="instagram" /></a>
             </div>
         </div>
